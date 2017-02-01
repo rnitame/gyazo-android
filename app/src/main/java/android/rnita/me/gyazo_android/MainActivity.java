e61389fe77f071e45b3495b183fe968ca0680831
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-02-02 01:40:11 +0900

    add: setting menu

--- app/src/main/java/android/rnita/me/gyazo_android/MainActivity.java
+++ app/src/main/java/android/rnita/me/gyazo_android/MainActivity.java
@@ -1,7 +1,9 @@
 package android.rnita.me.gyazo_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
