9bea8b15eb337dfd515cea071c769969ed52c545
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-02-02 02:19:11 +0900

    modify: enable select picture from gallery

--- app/src/main/java/android/rnita/me/gyazo_android/MainActivity.java
+++ app/src/main/java/android/rnita/me/gyazo_android/MainActivity.java
@@ -1,16 +1,34 @@
 package android.rnita.me.gyazo_android;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AppCompatButton selectPictureButton;
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectPictureButton = (AppCompatButton) findViewById(R.id.select_picture);
+        selectPictureButton.setOnClickListener(this);
     }
 
     @Override
@@ -18,5 +36,38 @@ public class MainActivity extends AppCompatActivity {
         final MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000 && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                try {
+                    Bitmap bitmap = getBitmapFromUri(data.getData());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return bitmap;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+
+        startActivityForResult(intent, 1000);
     }
 }
