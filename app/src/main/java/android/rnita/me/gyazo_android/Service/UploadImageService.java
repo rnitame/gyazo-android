94ea9554c4f8ff4576c93efbf3d341995d802e53
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-02-05 15:28:08 +0900

    add: UploadImageService

---
+++ app/src/main/java/android/rnita/me/gyazo_android/Service/UploadImageService.java
@@ -0,0 +1,16 @@
+package android.rnita.me.gyazo_android.Service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class UploadImageService extends IntentService {
+
+    public UploadImageService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
